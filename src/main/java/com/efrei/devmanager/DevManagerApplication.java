243d15867b2c31082ddb38cd3675c7557ceaea7d
theMatuvu <matthieuju1@gmail.com>
2025-12-12 10:11:41 +0100

    init project

---
+++ src/main/java/com/efrei/devmanager/DevManagerApplication.java
@@ -0,0 +1,13 @@
+package com.efrei.devmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/efrei/devmanager/DevManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.devmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
