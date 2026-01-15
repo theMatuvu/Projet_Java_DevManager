ad85371c8264a845d43ce410836bf7f595c01130
theMatuvu <matthieuju1@gmail.com>
2026-01-15 11:11:50 +0100

    add delete and update salary Programmeur

---
+++ src/main/java/com/efrei/devmanager/InterfaceChoix.java
@@ -0,0 +1,13 @@
+package com.efrei.devmanager;
+
+public interface InterfaceChoix {
+    void printProgrammeurs();
+    void printProgrammeur();
+    void deleteProgrammeur();
+    void addProgrammeur();
+    void updateProgrammeurSalaire();
+    void printProjectList();
+    void programmerListByProject();
+    void exit();
+
+}
