d872e291c4bd158294e2bf15bf6290ccf80ab1b4
theMatuvu <matthieuju1@gmail.com>
2026-01-15 11:22:29 +0100

    edit InterfaceChoix

--- src/main/java/com/efrei/devmanager/InterfaceChoix.java
+++ src/main/java/com/efrei/devmanager/InterfaceChoix.java
@@ -2,12 +2,12 @@ package com.efrei.devmanager;
 
 public interface InterfaceChoix {
     void printProgrammeurs();
-    void printProgrammeur();
-    void deleteProgrammeur();
-    void addProgrammeur();
-    void updateProgrammeurSalaire();
+    void printProgrammeur(int id);
+    void deleteProgrammeur(int id);
+    void addProgrammeur(Programmeur programmeur);
+    void updateProgrammeurSalaire(int id, double salaire);
     void printProjectList();
-    void programmerListByProject();
+    void programmerListByProject(int id);
     void exit();
 
 }
